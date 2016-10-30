53bf6c4815072013abf8297f8ed95e11b93e7d99
Le Hai Nam <lehainamm@gmail.com>
2016-10-30 15:37:18 +0700

    Move the example tutorial initialization to ViewDidLoad

--- SpotlightExample/SpotlightExample/ViewController.m
+++ SpotlightExample/SpotlightExample/ViewController.m
@@ -18,16 +18,10 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
-
-
-}
-
--(void)viewDidAppear:(BOOL)animated{
-  [super viewDidAppear:animated];
 
   spotlightController=[[FFSpotLightView alloc] initInView:self.view];
 
+  //The properties of the guideline view (width, arrow direction, shape,..) can be set random order
   [[[[[[spotlightController addGuidelineForView:self.button] setShape:FFSpotlightShapeRectangle] setInstruction:@"This is a button with rectangle spotlight shape, 400pt width and down direction tooltip view arrow"] setWidth:400] setArrowDirection:JDFTooltipViewArrowDirectionDown] submitGuide];
 
   [[[[[spotlightController addGuidelineForView:self.button2] setShape:FFSpotlightShapeOval] setInstruction:@"This is a button with oval spotlight shape and up direction tooltip view arrow"] setArrowDirection:JDFTooltipViewArrowDirectionLeft]submitGuide];
@@ -38,12 +32,13 @@
 
 }
 
-
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
   spotlightController=nil;
 }
+
+//Move to next guideline
 - (IBAction)button1:(id)sender {
   [spotlightController moveToNextGuide];
 
